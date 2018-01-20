b5f2d80e822e02d08317443d598491e8e5cc2982
North <rhearnorth@gmail.com>
2018-01-20 13:57:34 +0700

    Set root as sign-in path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :contract_plans
-  root to: 'visitors#index'
+  resources :contract_plans do
+    post :buy
+  end
   devise_for :users
   resources :users
+
+  authenticated :user do
+    root "contract_plans#index", as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
