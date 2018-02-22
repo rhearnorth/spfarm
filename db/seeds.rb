3c2a7109dcc18088d651411749e1ef781ef23b1a
North V <rhearnorth@gmail.com>
2018-02-22 22:08:36 +0700

    Seed contract plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,28 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+
+contract_plans = [
+  {
+    hash_rate: 20,
+    price: 30000,
+    contract_long: 24
+  },
+  {
+    hash_rate: 50,
+    price: 71250,
+    contract_long: 24
+  },
+  {
+    hash_rate: 100,
+    price: 136500,
+    contract_long: 24
+  },
+  {
+    hash_rate: 410,
+    price: 535050,
+    contract_long: 24
+  },
+]
+ContractPlan.create(contract_plans)
+puts "CREATE ContractPlan"
